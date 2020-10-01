7a7dac17f6bd81750a8d61d22fb0a6a3b9b657b7
JJ <jasonjinhang@hotmail.com>
2020-09-30 17:16:55 -0700

    first commit

---
+++ snake2.js
@@ -0,0 +1,250 @@
+var content = document.getElementById('content');
+var startPage = document.getElementById('startPage');
+var scoreBox = document.getElementById('score');
+var losePage = document.getElementById('losePage');
+var finalScore = document.getElementById('finalScore');
+var close = document.getElementById('close');
+var startOrPause = document.getElementById('startOrPause');
+var startGameBtn = document.getElementById('startGameBtn');
+var startGameBool = true;
+var startPauseBool = true;
+var snakeMove;
+var speed = 200;
+
+init();
+function init () {
+    // init map
+    this.mapWidth = parseInt(getComputedStyle(content).width);
+    this.mapHeight = parseInt(getComputedStyle(content).height);
+    this.mapDiv = content;
+    // init food
+    this.foodWidth = 20;
+    this.foodHeight = 20;
+    this.foodX = 0;
+    this.foodY = 0;
+    // init snake
+    this.snakeWidth = 20;
+    this.snakeHeight = 20;
+    this.snakeBody = [[3,1,'head'],[2,1,'body'],[1,1,'body']];
+    // game property
+    this.direct = 'right';
+    this.left = false;
+    this.right = false;
+    this.up = true;
+    this.down = true;
+    this.score = 0;
+    bindEvent();
+}
+function startGame() {
+    startPage.style.display = 'none';
+    startOrPause.style.display = 'block';
+    food();
+    snake();
+
+}
+function food() {
+    var food = document.createElement('div');
+    food.style.width = this.foodWidth + 'px';
+    food.style.height = this.foodHeight + 'px';
+    food.style.position = 'absolute';
+    this.foodX = Math.floor(Math.random()*(this.mapWidth/20));
+    this.foodY = Math.floor(Math.random()*(this.mapHeight/20));
+    food.style.left = this.foodX * 20 + 'px';
+    food.style.top = this.foodY * 20 + 'px';
+    this.mapDiv.appendChild(food).setAttribute('class', 'food');
+}
+function snake () {
+    for (var i = 0; i < this.snakeBody.length; i++) {
+        var snake = document.createElement('div');
+        snake.style.width = this.snakeWidth + 'px';
+        snake.style.height = this.snakeHeight + 'px';
+        snake.style.position = 'absolute';
+        snake.style.left = this.snakeBody[i][0] * 20 + 'px';
+        snake.style.top = this.snakeBody[i][1] * 20 + 'px';
+        snake.classList.add(this.snakeBody[i][2]);
+        this.mapDiv.appendChild(snake).classList.add('snake');
+        switch (this.direct) {
+            case 'right':
+                break;
+            case 'down':
+                snake.style.transform = 'rotate(90deg)';
+                break;
+            case 'left':
+                snake.style.transform = 'rotate(180deg)';
+                break;
+            case 'up':
+                snake.style.transform = 'rotate(270deg)';
+                break;
+            default:
+                break;
+        }
+    }
+}
+function move () {
+    for (var i = this.snakeBody.length-1; i > 0; i--){
+        this.snakeBody[i][0] = this.snakeBody[i-1][0]; // pre replaces the current position x
+        this.snakeBody[i][1] = this.snakeBody[i-1][1]; // pre replaces the current position y
+    }
+    switch (this.direct) { // move snake or say snake's head
+        case 'right':
+            this.snakeBody[0][0] += 1;
+            break;
+        case 'down':
+            this.snakeBody[0][1] += 1;
+            break;
+        case 'left':
+            this.snakeBody[0][0] -= 1;
+            break;
+        case 'up':
+            this.snakeBody[0][1] -= 1;
+            break;
+        default:
+            break;
+    }
+    removeClass('snake');
+    snake();
+    if (this.snakeBody[0][0] == this.foodX && this.snakeBody[0][1] == this.foodY) {
+        var snakeEndX = this.snakeBody[this.snakeBody.length - 1][0];
+        var snakeEndY = this.snakeBody[this.snakeBody.length - 1][1];
+        switch (this.direct) {
+            case 'right':
+                this.snakeBody.push([snakeEndX - 1,snakeEndY,'body']);
+                break;
+            case 'left':
+                this.snakeBody.push([snakeEndX + 1,snakeEndY,'body']);
+                break;
+            case 'up':
+                this.snakeBody.push([snakeEndX,snakeEndY + 1,'body']);
+                break;
+            case 'down':
+                this.snakeBody.push([snakeEndX,snakeEndY - 1,'body']);
+                break;
+            default:
+                break;
+        }
+        this.score += 1;
+        scoreBox.innerHTML = this.score;
+        removeClass('food');
+        food();
+    }
+    if (this.snakeBody[0][0] < 0 || this.snakeBody[0][0] >= this.mapWidth/20) {
+        reloadGame();
+        // console.log('xxx');
+    }
+    if (this.snakeBody[0][1] < 0 || this.snakeBody[0][1] >= this.mapHeight/20) {
+        reloadGame();
+        // console.log('yyy');
+    }
+    var snakeHeadX = this.snakeBody[0][0];
+    var snakeHeadY = this.snakeBody[0][1];
+    for (var i = 1; i < this.snakeBody.length; i++) {
+        if(snakeHeadX == this.snakeBody[i][0] && snakeHeadY == this.snakeBody[i][1]) {
+            reloadGame();
+            // console.log('self');
+        }
+    }
+}
+function reloadGame () {
+    removeClass('food');
+    removeClass('snake');
+    clearInterval(snakeMove);
+    this.snakeBody = [[3,1,'head'],[2,1,'body'],[1,1,'body']];
+    this.direct = 'right';
+    this.left = false;
+    this.right = false;
+    this.up = true;
+    this.down = true;
+    losePage.style.display = 'block';
+    finalScore.innerHTML = this.score;
+    this.score = 0;
+    scoreBox.innerHTML = this.score;
+    startGameBool = true;
+    startPauseBool = true;
+    startOrPause.setAttribute('src','snakeImage/startButton.png');
+}
+function removeClass (className) {
+    var ele = document.getElementsByClassName(className);
+    while (ele.length > 0) {
+        ele[0].parentNode.removeChild(ele[0]);
+    }
+}
+function bindEvent () {
+    close.onclick = function () {
+        losePage.style.display = 'none';
+    }
+    startGameBtn.onclick = function () {
+        console.log("actived!");
+        startAndPause();
+    }
+    startOrPause.onclick = function () {
+        console.log("actived!");
+        startAndPause();
+    }
+}
+function setDirect (code) {
+    switch(code) {
+        case 37:
+            if (this.left) {
+                this.direct = 'left';
+                this.left = false;
+                this.right = false;
+                this.up = true;
+                this.down = true;
+            }
+            break;
+        case 38:
+            if (this.up) {
+                this.direct = 'up';
+                this.left = true;
+                this.right = true;
+                this.up = false;
+                this.down = false;
+            }
+            break;
+        case 39:
+            if (this.right) {
+                this.direct = 'right';
+                this.left = false;
+                this.right = false;
+                this.up = true;
+                this.down = true;
+            }
+            break;
+        case 40:
+            if (this.down) {
+                this.direct = 'down';
+                this.left = true;
+                this.right = true;
+                this.up = false;
+                this.down = false;
+            }
+            break;
+        default:
+            break;
+    }
+}
+function startAndPause() {
+    if (startPauseBool) {
+        if(startGameBool) {
+            startGame();
+            startGameBool = false;
+        }
+        startOrPause.setAttribute('src','snakeImage/pauseButton.png');
+        document.onkeydown = function (e) {
+            var code = e.keyCode;
+            setDirect(code);
+        }
+        snakeMove = setInterval(function(){
+            move();
+        }, speed);
+        startPauseBool = false;
+    } else {
+        startOrPause.setAttribute('src','snakeImage/startButton.png');
+        clearInterval(snakeMove); // snake stops moving
+        document.onkeydown = function (e) {
+            e.returnValue = false;
+            return false;
+        };
+        startPauseBool = true;
+    }
+}
